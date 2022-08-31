3629b7d9a763f70656efb5e5bf114894607e13b2
officialsmp305 <Officialsmp305@gmail.com>
2022-08-31 12:47:23 -0700

    [FRONTEND]: Navbar Component & Styles.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { BsMoon } from "react-icons/bs";
+import { ImSun } from "react-icons/im";
+import "../scss/components/NavBar.scss";
+
+export default function NavBar({ changeTheme, currentTheme }) {
+  const [theme, setTheme] = useState(false);
+
+  return (
+    <header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="header-inner">
+            <div className="logo">
+              <span>VelvetSunshine Co.</span>
+              <li onClick={changeTheme} className="theme-mode">
+                {currentTheme === "dark" ? (
+                  <ImSun className="sun" />
+                ) : (
+                  <BsMoon className="moon" />
+                )}
+              </li>
+            </div>
+            <div className="navigation">
+              <nav>
+                <ul className="nav_list_grid">
+                  {["Home", "Shop", "Collections", "About"].map((item) => (
+                    <li className="nav_item" key={`link-${item}`}>
+                      <div />
+                      <a className="nav_link" href={`#${item}`}>
+                        {item}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            <div className="nav-login">
+              <div className="login">
+                <a href="/">Log In</a>
+              </div>
+              <div className="cart">
+                <a href="/">Cart</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
